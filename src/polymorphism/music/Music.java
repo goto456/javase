e8084bfadc9f8086f997d2a559a9d64c76024fa1
biwen <wangbiwen_88@qq.com>
2015-10-19 17:55:15 +0800

    Create a project and some class doc.

---
+++ src/polymorphism/music/Music.java
@@ -0,0 +1,12 @@
+package polymorphism.music;
+
+public class Music {
+  public static void tune(Instrument i) {
+    i.play(Note.MIDDLE_C);
+  }
+  
+  public static void main(String[] args) {
+    Wind wind = new Wind();
+    tune(wind);
+  }
+}
---
+++ src/polymorphism/music/Wind.java
@@ -0,0 +1,7 @@
+package polymorphism.music;
+
+public class Wind extends Instrument {
+  public void play(Note n) {
+    System.out.println("Wind.play() " + n);
+  }
+}
