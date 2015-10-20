98672a531a51735a291671cd5edcc475173f10f5
biwen <wangbiwen_88@qq.com>
2015-10-20 11:33:20 +0800

    Create some java code.

--- src/polymorphism/music/Note.java
+++
@@ -1,5 +0,0 @@
-package polymorphism.music;
-
-public enum Note {
-  MIDDLE_C, C_SHARP, B_FLAT;
-}
--- src/polymorphism/music/Wind.java
+++
@@ -1,7 +0,0 @@
-package polymorphism.music;
-
-public class Wind extends Instrument {
-  public void play(Note n) {
-    System.out.println("Wind.play() " + n);
-  }
-}
