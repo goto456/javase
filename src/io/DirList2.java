ecf670e3699056b7b1487109633140069179e03d
biwen <wangbiwen_88@qq.com>
2015-10-29 17:42:44 +0800

    Create som java code file in io and net.midview.util

--- src/io/DirList2.java
+++ src/io/DirList2.java
@@ -5,32 +5,30 @@ import java.io.FilenameFilter;
 import java.util.Arrays;
 import java.util.regex.Pattern;
 
-
-public class DirList {
+public class DirList2 {
+  public static FilenameFilter filter(final String regex) {
+    return new FilenameFilter() {
+      private Pattern pattern = Pattern.compile("regex");
+      @Override
+      public boolean accept(File dir, String name) {
+        return pattern.matcher(name).matches();
+      }
+    };
+  }
+  
   public static void main(String[] args) {
+    //System.out.println(args[0]);
     File path = new File(".\\src\\io");
     String[] list;
     if (args.length == 0) {
       list = path.list();
     }
     else {
-      list = path.list(new DirFilter(args[0]));
+      list = path.list(filter(args[0]));
     }
     Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
     for (String dirItem : list) {
       System.out.println(dirItem);
     }
-  }
-}
-
-class DirFilter implements FilenameFilter {
-  private Pattern pattern;
-  
-  public DirFilter(String regex) {
-    pattern = Pattern.compile(regex);
-  }
-  
-  public boolean accept(File dir, String name) {
-    return pattern.matcher(name).matches();
   }
 }
---
+++ src/net/mindview/util/ProcessFiles.java
@@ -0,0 +1,5 @@
+package net.mindview.util;
+
+public class ProcessFiles {
+
+}
