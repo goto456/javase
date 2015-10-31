5fe2723179d57b9020211001db476e038860a702
Wang Biwen <wangbiwe_88@qq.com>
2015-10-31 23:16:02 +0800

    change some code

--- src/net/mindview/util/ProcessFiles.java
+++ src/net/mindview/util/ProcessFiles.java
@@ -1,5 +1,40 @@
 package net.mindview.util;
 
-public class ProcessFiles {
+import java.io.File;
+import java.io.IOException;
 
+public class ProcessFiles {
+  public interface Strategy {
+    void process(File file);
+  }
+  
+  private Strategy strategy;
+  private String ext;
+  
+  public ProcessFiles(Strategy strategy, String ext) {
+    this.strategy = strategy;
+    this.ext = ext;
+  }
+  
+  public void processDirectoryTree(File root) throws IOException {
+    for (File file : Directory.walk(
+        root.getAbsolutePath(), ".*\\." + ext)) {
+      strategy.process(file.getCanonicalFile());
+    }
+  }
+  
+  public void start(String[] args) {
+    try {
+      if (args.length == 0) {
+        processDirectoryTree(new File("."));
+      }
+      else {
+        
+      }
+    }
+  }
+  
+  
+  
+  
 }
