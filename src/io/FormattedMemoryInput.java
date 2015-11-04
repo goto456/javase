acdb245b218907a239b3aea8f9c270c2b074fa57
biwen <wangbiwen_88@qq.com>
2015-11-04 21:39:48 +0800

    add some java code file.

---
+++ src/io/FormattedMemoryInput.java
@@ -0,0 +1,13 @@
+package io;
+
+import java.io.IOException;
+
+public class FormattedMemoryInput {
+  public static void main(String[] args) throws IOException {
+    try {
+      
+    } catch (Exception e) {
+      // TODO: handle exception
+    }
+  }
+}
