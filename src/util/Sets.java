60c36697acf3a4f12e2b85d3c7c4791e77bee169
wangbiwen [王必文] <wangbiwen@xiaomi.com>
2017-01-19 10:03:53 +0800

    add code.

---
+++ src/util/Sets.java
@@ -0,0 +1,35 @@
+package util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * FileName: Sets.java
+ * Description:
+ * Authors: wangbiwen
+ * Date: 17-1-9
+ */
+public class Sets {
+    public static <T> Set<T> union(Set<T> a, Set<T> b) {
+        Set<T> result = new HashSet<T>(a);
+        result.addAll(b);
+        return result;
+    }
+
+    public static <T> Set<T> intersection(Set<T> a, Set<T> b) {
+        Set<T> result = new HashSet<T>(a);
+        result.retainAll(b);
+        return result;
+    }
+
+    public static <T> Set<T> difference(Set<T> superset, Set<T> subset) {
+        Set<T> result = new HashSet<T>(superset);
+        result.removeAll(subset);
+        return result;
+    }
+
+    public static <T> Set<T> complement(Set<T> a, Set<T> b) {
+        return (difference(union(a, b), intersection(a, b)));
+    }
+
+}
