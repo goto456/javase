6d089feef143cd1536c13fab70217c8b71da2a89
wangbiwen [王必文] <wangbiwen@xiaomi.com>
2017-02-24 09:41:29 +0800

    add code.

---
+++ src/generics/HasF.java
@@ -0,0 +1,13 @@
+package generics;
+
+/**
+ * FileName: HasF.java
+ * Description:
+ * Authors: wangbiwen
+ * Date: 17-1-17
+ */
+public class HasF {
+    public void f() {
+        System.out.println("HasF.f()");
+    }
+}
